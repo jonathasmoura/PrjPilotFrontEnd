626070d2a893f78047f5ed55e2597b3f29b580f6
Jonathas Moura <jonathas.development@gmail.com>
2024-10-26 02:14:27 -0300

    Criação de componente e services para Categorias
    Inclusão de componentes para Anúncios e Categorias.
    Inclusão de Links no menu nav.
    Inclusão do http Client para comunicação com API.
    Inclusão de Service para integração de backend.

--- PrjPilot.UI/src/app/app.module.ts
+++ PrjPilot.UI/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './users/login/login.component';
 import { RegisterComponent } from './users/register/register.component';
 import { ListProductsComponent } from './components/products/list-products/list-products.component';
+import { ListAdvertisementsComponent } from './components/advertisements/list-advertisements/list-advertisements.component';
+import { ListCategoriesComponent } from './components/categories/list-categories/list-categories.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    ListProductsComponent
+    ListProductsComponent,
+    ListAdvertisementsComponent,
+    ListCategoriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ PrjPilot.UI/src/app/services/categories.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from '../models/category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+
+   baseUrl = 'https://localhost:44351/Category/';
+  constructor(private http: HttpClient) { 
+    
+  }
+
+
+
+  getAllCategories(): Observable<Category[]>{
+    return this.http.get<Category[]>(this.baseUrl + 'categories');
+}
+
+}

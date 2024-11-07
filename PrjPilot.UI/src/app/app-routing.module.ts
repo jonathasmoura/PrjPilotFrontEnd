87086e3cb2b349d7dfa108a56c512edc743d851b
Jonathas Moura <jonathas.development@gmail.com>
2024-11-07 01:34:02 -0300

    Implementações editar produto
    Incluído rota, para edição de produto.
    Incluído Componente para editar produto no app module.
    Incluído disabled quando formulário estiver preenchido de forma correte e botão voltar com método que retorna para lista de produtos.
    Incluído no componente de adicionar produto, método que retorna para lista de produtos.
    Criado formulário para editar produto.
    Criado métodos responsáveis pelas ações de editar um produto.
    Incluído botão de editar um produto na lista de produtos.
    Refatorado classe de Produto, antes atributo era string, agora é um number.
    Inclusão de métodos na camada de serviço, para obter um produto por id e atualizar um produto, com suas respectivas rotas para os endpoints no backend.

--- PrjPilot.UI/src/app/app-routing.module.ts
+++ PrjPilot.UI/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RegisterComponent } from './users/register/register.component';
 import { ListProductsComponent } from './components/products/list-products/list-products.component';
 import { ListCategoriesComponent } from './components/categories/list-categories/list-categories.component';
 import { AddproductComponent } from './components/products/addproduct/addproduct.component';
+import { EditproductComponent } from './components/products/editproduct/editproduct.component';
 
 const routes: Routes = [
   {
@@ -26,6 +27,10 @@ const routes: Routes = [
     {
       path:'incluir-produto',
       component:AddproductComponent
+    },
+    {
+      path:'editar-produto/:id',
+      component:EditproductComponent
     }
 ];
 
